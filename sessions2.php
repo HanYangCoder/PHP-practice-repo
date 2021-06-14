611e692eab691d6e976cf53641e52318ac2088cd
Hans Christian Herrera <hanschristian.herrera@wvsu.edu.ph>
2021-06-14 15:58:55 +0800

    Tried adding PHP if conditions using sessions  with HTML outputs

--- sessions2.php
+++ sessions2.php
@@ -7,9 +7,20 @@
 
 <?php
     // Echo session variables that were set on previous page
-    echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-    echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+    // echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+    // echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
 ?>
+
+<!-- code tested below is a PHP if statement which executes an HTML output
+    the variable in the if statement is a PHP session variable set
+    in sessions1.php -->
+<?php if ($_SESSION["favcolor"] != "green"): ?>
+    <p>The statement is true</p>
+<?php else: ?>
+    <p>The statement is false</p>
+<?php endif; ?>
+
+<?php print_r($_SESSION); ?>
 
 </body>
 </html> 
--- sessions3.php
+++ sessions3.php
@@ -6,6 +6,7 @@
 <body>
 
 <?php
+    
     // Echo session variables that were set on previous page
     //echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
     //echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
